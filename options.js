7aff780e950b41928c898adebe310d2a1a6ddcf1
maykham <may.yaaron@gmail.com>
2016-10-09 16:19:00 +0300

    lost vid design

--- options.js
+++ options.js
@@ -2,8 +2,9 @@
 var url = location.search.match(/=(.+)/).pop();
 // document.querySelector('body').innerHTML = url;
 
-videojs("video", {}, function() {
-    console.log(this)
-    // this.play()
-
+videojs("video", {
+	playbackRates: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2], autoplay: true
+}, function() {
+    console.log(this);
+    this.src(url);
 });
